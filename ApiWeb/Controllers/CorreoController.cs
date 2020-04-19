be09ba6c7515f0308b32dcd3814f5a64f61e8be8
RojasLinaresRuben <ruben.rojaslinares@gmail.com>
2020-04-19 17:25:56 -0500

    Inicio api controller Remoto vs 2019

---
+++ ApiWeb/Controllers/CorreoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiWeb.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    [EnableCors]
+    public class CorreoController : ControllerBase
+    {
+        [HttpGet]
+        public object GetPersona()
+        {
+           
+            Persona persona = new Persona {
+                Apellido = "Rojas",
+                Edad = 32,
+                Nombre = "Ruben"
+            };
+            return persona;
+        }
+
+        //[HttpGet]
+        //public object GetAlumno(int id)
+        //{
+        //    return "no tenemos nada que ocultar";
+        //}
+    }
+}
---
+++ ApiWeb/Model/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWeb.Model
+{
+    public class Persona
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+    }
+}
